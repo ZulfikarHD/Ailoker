fefb9a59246e79e336db7b9110ffb568999d9ba9
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-05-28 11:15:20 +0700

    setting migration company contact

--- database/migrations/2024_05_28_035221_create_company_contact_table.php
+++ database/migrations/2024_05_28_035221_create_company_contact_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('company_contact', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone_number')->nullable();
             $table->timestamps();
         });
     }
