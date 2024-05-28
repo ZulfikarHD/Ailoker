e4db6f88eee79b8547aa788a7faf0b4f88252d24
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-05-28 10:44:40 +0700

    install tiny

---
+++ resources/views/admin/form/posting-loker.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+	<div class="max-w-sm px-4 md:max-w-3xl lg:px-10">
+		<div class="flex flex-col gap-4 px-8 py-4 bg-white rounded-lg shadow-md">
+
+		</div>
+	</div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,8 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::view('admin/form/posting-loker', 'admin.form.posting-loker')
+    // ->middleware(['auth'])
+    ->name('admin.form.posting-loker');
+
 require __DIR__.'/auth.php';
