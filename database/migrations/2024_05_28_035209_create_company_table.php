525dcedfb3bf648a7bab8412e3543263864f3c97
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-05-28 11:15:43 +0700

    setting migration company

--- database/migrations/2024_05_28_035209_create_company_table.php
+++ database/migrations/2024_05_28_035209_create_company_table.php
@@ -13,7 +13,20 @@ return new class extends Migration
     {
         Schema::create('company', function (Blueprint $table) {
             $table->id();
+            $table->string('cmp_name');
+            $table->string('cmp_address');
+            $table->unsignedBigInteger('industry_id');
+            $table->text('cmp_size')->nullable();
+            $table->string('website')->nullable();
+            $table->text('about_us')->nullable();
+            $table->string('cmp_logo')->nullable();
             $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('industry_id')
+                  ->references('id')
+                  ->on('industry')
+                  ->onDelete('cascade');
         });
     }
 
