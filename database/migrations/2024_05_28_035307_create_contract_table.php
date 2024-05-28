59d804c0a1a1b30a356ff387dd1941e8c5b1f3f9
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-05-28 13:23:17 +0700

    setting contract

--- database/migrations/2024_05_28_035307_create_contract_table.php
+++ database/migrations/2024_05_28_035307_create_contract_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('contract', function (Blueprint $table) {
             $table->id();
+            $table->string('duration', 255);
             $table->timestamps();
         });
     }
