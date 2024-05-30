d707b239847d61bb83706481fbbc923c407ad242
ZulfikarHD <zulfikar.h9050@gmail.com>
2024-05-30 16:36:06 +0700

    add section job title

--- app/Http/Controllers/Admin/PostLokerController.php
+++ app/Http/Controllers/Admin/PostLokerController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Contract;
 
 class PostLokerController extends Controller
 {
     public function index()
     {
-        return view('admin.form.posting-loker');
+        $contracts = Contract::all();
+
+        return view('admin.form.posting-loker',[
+            'contracts' => $contracts,
+        ]);
     }
 
     public function store(Request $request)
